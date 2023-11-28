0c1bbd3e3f3ab47b1418fba3926b69dfaa3586ec
Marwa Jaberallah Sabon <marwasabon@gmail.com>
2023-11-28 19:20:37 +0200

    Create 4-binary_tree_is_leaf.c

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - checks - if the node is a leaf
+ * @node: pointer to a node.
+ *
+ * Return: 1 if leaf else 0
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (!node)
+		return (0);
+	if (!node->left && !node->right)
+		return (1);
+	return (0);
+}
